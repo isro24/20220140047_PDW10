ea5bdaf49771eae3604e463f739a00c1cddad939
isro24 <alamat_email_anda@example.com>
2024-06-12 13:21:06 +0700

    update

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ function kirimData(event) {
     
     var name = document.getElementById("nama").value;
     var nim = document.getElementById("nim").value;
-    var peminatan = document.querySelector("input[name=pemintatan]:checked").value;
+    var peminatan = document.querySelector("input[name=peminatan]:checked").value;
     var alamat = document.getElementById("alamat").value;
+    var angkatan = document.getElementById("angkatan").value;
+    var tanggal = document.getElementById("tanggal").value;
     
     Swal.fire({
         title: 'Data Submitted',
@@ -13,6 +15,8 @@ function kirimData(event) {
             <p><strong>NIM:</strong> ${nim}</p>
             <p><strong>Peminatan:</strong> ${peminatan}</p>
             <p><strong>Alamat:</strong> ${alamat}</p>
+            <p><strong>Angkatan:</strong> ${angkatan}</p>
+            <p><strong>Tanggal:</strong> ${tanggal}</p>
         `,
         icon: 'success',
         confirmButtonText: 'OK'
